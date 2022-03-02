2f5c006837dc830d1c5912314505c913fd8e41eb
Thiago <thiagomcosta@yahoo.com.br>
2022-03-02 12:14:44 -0300

    Criação de classe ExcelResultColumns Classe utilizada para relacionar as colunas de Excel com as colunas de saída

---
+++ Excel2Txt/ExcelResult/ExcelResultColumns.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Excel2Txt
+{
+    class ExcelResultColumns
+    {
+        public Dictionary<int, string> ColumnsTitles = new Dictionary<int, string>();
+
+        public void Add(ExcelColumn column, string Title)
+        {
+            if(SearchTitle(Title) > 0)
+            {
+                throw new Exception("Valor já existe em ExcelResultColumns");
+            }
+            if (this.ColumnsTitles.ContainsKey(column.Number)) 
+            {
+                this.ColumnsTitles[column.Number] = Title;
+                return;
+            }
+            this.ColumnsTitles.Add(column.Number, Title);
+        }
+
+        public int SearchTitle(string Title)
+        {
+            KeyValuePair<int, string> ResultColumn = this.ColumnsTitles.Where(item => item.Value == Title).FirstOrDefault();
+            if(ResultColumn.Value == null)
+            {
+                return 0;
+            }
+            return ResultColumn.Key;
+        }
+
+        public void Remove(ExcelColumn column)
+        {
+            this.ColumnsTitles.Remove(column.Number);
+        }
+
+        public void Remove(string Title)
+        {
+            KeyValuePair<int, string> ResultColumn = this.ColumnsTitles.Where(item => item.Value == Title).FirstOrDefault();
+            if (ResultColumn.Value != null)
+            {
+                this.ColumnsTitles.Remove(ResultColumn.Key);
+            }
+        }
+
+        public List<string> TitleList()
+        {
+            return this.ColumnsTitles.Values.Select(item => item).ToList();
+        }
+
+    }
+}
